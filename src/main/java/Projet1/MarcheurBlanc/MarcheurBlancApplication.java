953b00a42642611049d0e1b28b8fd5c1fcda30f5
Maharavo1 <hei.maharavo@gmail.com>
2024-06-17 15:27:27 +0300

    feat: Initial commit

---
+++ src/main/java/Projet1/MarcheurBlanc/MarcheurBlancApplication.java
@@ -0,0 +1,16 @@
+package Projet1.MarcheurBlanc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.*;
+
+@SpringBootApplication
+public class MarcheurBlancApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcheurBlancApplication.class, args);
+
+
+
+}}
---
+++ src/test/java/Projet1/MarcheurBlanc/MarcheurBlancApplicationTests.java
@@ -0,0 +1,16 @@
+package Projet1.MarcheurBlanc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootTest
+class MarcheurBlancApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
